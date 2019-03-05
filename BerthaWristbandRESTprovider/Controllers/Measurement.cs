e27ffcdda33b661e8b5c86df6b95f9e1028bf6e1
Anders Børjesson <anbo@zealand.dk>
2019-03-05 18:32:44 +0100

    property names changed

---
+++ BerthaWristbandRESTprovider/Controllers/Measurement.cs
@@ -0,0 +1,14 @@
+﻿namespace BerthaWristbandRESTprovider.Controllers
+{
+    public class Measurement
+    {
+        public int DeviceId { get; set; }
+        public double Pm25 { get; set; }
+        public double Pm10 { get; set; }
+        public int Co2 { get; set; }
+        public int O3 { get; set; }
+        public double Pressure { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+    }
+}
---
+++ BerthaWristbandRESTprovider/Controllers/WristbandDataController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+// TODO clean up: remove unused methods
+namespace BerthaWristbandRESTprovider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WristbandDataController : ControllerBase
+    {
+        private static int nextId = 1;
+        private static Random random = new Random();
+        private static int deviceId = random.Next();
+
+        // GET: api/WristbandData
+        [HttpGet]
+        public Measurement Get()
+        {
+            Measurement m = new Measurement()
+            { // TODO data relative to former measurement
+                DeviceId = deviceId,
+                Pm25 = random.Next(50, 120) / 10.0,
+                Pm10 = random.Next(100, 500) / 10.0,
+                Co2 = random.Next(400, 500),
+                O3 = random.Next(200000, 300000),
+                Pressure = random.Next(100000, 105000) / 100.0,
+                Humidity = random.Next(20, 100),
+                Temperature = random.Next(-2000, 5000) / 100.0   
+            };
+            return m;
+        }
+
+        // GET: api/WristbandData/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/WristbandData
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/WristbandData/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
